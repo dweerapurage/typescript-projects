5a7c3bd589849fe80d3ac969467c7bb7685d5021
Dinesh Weerapurage <dweerapurage@salesforce.com>
2025-04-18 10:37:27 -0600

    adding diamond implementation

--- diamonds/src/main.ts
+++ diamonds/src/main.ts
@@ -1,21 +1,21 @@
 
 
-function diamond(size: number): string {
+function diamond (size: number): string {
     let dia : string[] = [];
-    for (let i = 0; i < size; i++) {
+
+    // Top of the diamond
+    for (let i = 0; size > i; i++) {
         let t = size - i - 1;
-        for (let j = 0; j < size; j++) {
+        for (let j = 0; size >= j; j++) {
             if ( j > t) {
-                // console.log(`${i},${j}: /`);
                 dia.push("/");
             } else {
-                // console.log(`${i},${j}: `);
                 dia.push(" ");
             }
         }
 
-        for (let k = 0; k < size; k++) {
-            if (k > i) {
+        for (let k = 0; size >= k; k++) {
+            if (i >= k) {
                 dia.push("\\")
             } else {
                 dia.push(" ")
@@ -23,12 +23,89 @@ function diamond(size: number): string {
         }
         dia.push("\n");
     }
+
+    // Bottom of the diamond
+     for (let i = 0; size > i; i++) {
+        let t = size - i - 1;
+        for (let k = 0; size >= k; k++) {
+            if (k > i) {
+                dia.push("\\")
+            } else {
+                dia.push(" ")
+            }
+        }
+
+        for (let j = 0; size >= j; j++) {
+            if ( t >= j) {
+                // console.log(`${i},${j}: /`);
+                dia.push("/");
+            } else {
+                // console.log(`${i},${j}: `);
+                dia.push(" ");
+            }
+        }
+        dia.push("\n");
+    }
     return dia.join('');
 }
 
-function main() {
-    let message = diamond(4);
-    console.log(message);
+function diamond_empty (size: number): string {
+    let dia: string[] = [];
+    // Top of the diamond
+    for (let i = 0; i < size; i++) {
+        let t = size - i - 1;
+        for (let j = 0; j < size; j++) {
+            if ( j === t) {
+                dia.push("/");
+            } else {
+                dia.push(" ");
+            }
+        }
+
+        for (let k = 0; k < size; k++) {
+            if (i === k) {
+                dia.push("\\")
+            } else {
+                dia.push(" ")
+            }
+        }
+        dia.push("\n");
+    }
+
+    // Bottom of the diamond
+    for (let i = 0; i < size; i++) {
+        let t = size - i - 1;
+        for (let k = 0; k < size; k++) {
+            if (k === i) {
+                dia.push("\\")
+            } else {
+                dia.push(" ")
+            }
+        }
+
+        for (let j = 0; j < size; j++) {
+            if ( t === j) {
+                // console.log(`${i},${j}: /`);
+                dia.push("/");
+            } else {
+                // console.log(`${i},${j}: `);
+                dia.push(" ");
+            }
+        }
+        dia.push("\n");
+    }
+    return dia.join('');
+}
+
+function main () {
+    for (let c = 0; c < 10; c++) {
+        console.log(`Diamond size: ${c}`);
+        let e = diamond_empty(c)
+        console.log(e);
+        let d = diamond(c);
+        console.log(d);
+        console.log("====================");
+    }
 }
 
 main();
